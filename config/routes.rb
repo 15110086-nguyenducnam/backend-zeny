37ced999d5796b4eba9e9b147111e0ad263f1b46
Jerry <ducnam102@gmail.com>
2018-12-07 22:20:33 +0700

    analys

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {  sessions: 'admins/sessions',
                                       passwords: 'admins/passwords' }
   mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
+  resources :manage
 end
