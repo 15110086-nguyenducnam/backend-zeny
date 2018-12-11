63a6fe43e5d46bf0458a4987fba18c06692624f0
Jerry <ducnam102@gmail.com>
2018-12-12 01:43:46 +0700

    add khuyen mai

--- app/controllers/khuyenmais_controller.rb
+++ app/controllers/khuyenmais_controller.rb
@@ -1,37 +1,28 @@
 class KhuyenmaisController < ApplicationController
+  # before_action :check_day, only: [ :update, :create]
 
   def index
     @hopdong = Hopdongmuahang.includes(:chitiethdmuahangs).where(trangthaihd: "pending").decorate
-    @khuyenmai = Khuyenmai.new
+    @khuyenmai = Khuyenmai.where("ngaybd >= ? ", DateTime.now)
   end
 
   def create
     @khuyenmai = Khuyenmai.new(params_khuyenmai)
 
-    binding.pry
     if @khuyenmai.save
       flash[:success] = 'Create successfully!'
-      # redirect_to Something_path
+      redirect_to khuyenmais_path
     else
       flash.now[:danger] = @khuyenmai.errors.full_messages.join(', ')
+      # binding.pry
       redirect_to khuyenmais_path
     end
   end
-
-  def khuyenmai_loaisp
-    
-  end
-
-  def khuyenmai_sanpham
-    
-  end
-  
-  
   
   private
 
   def params_khuyenmai
-     params.require(:khuyenmai).permit(:ngaybd, :ngaykt, :loaisp)
+     params.require(:khuyenmai).permit(:ngaybd, :ngaykt, :sanpham)
   end
   
 
--- app/models/khuyenmai.rb
+++ app/models/khuyenmai.rb
@@ -1,3 +1,34 @@
 class Khuyenmai < ApplicationRecord
   belongs_to :sanpham
+  validates :ngaybd, :ngaykt, presence: true
+  validate :check_day
+
+  rails_admin do
+    label "Khuyến mãi"
+    label_plural "Khuyến mãi"
+    field :sanpham do
+      label "Sản phẩm"
+    end
+    field :ngaybd do
+      label "Ngày bắt đầu"
+    end
+    field :ngaykt do
+      label "Ngày kết thúc"
+    end
+    
+  end
+
+  private 
+
+  def check_day
+    # binding.pry
+    if self.ngaybd < DateTime.now
+      errors.add :ngaybd, "Ngày bắt đầu bé hơn ngày hiện tại..."
+    else
+      if self.ngaybd > self.ngaykt
+        errors.add :ngaykt, "Ngày kết thúc bé hơn ngày bắt đầu..."
+      end
+    end
+  end
+  
 end
